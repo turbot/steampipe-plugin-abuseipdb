7e1072d74c17b0b0451d9d456e973566de58b794
misraved <ved@turbot.com>
2022-09-28 13:33:08 +0530

    Fix lint failures

--- abuseipdb/utils.go
+++ abuseipdb/utils.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -42,7 +42,7 @@ func (c *abuseipdbClient) Get(ctx context.Context, path string, params url.Value
 
 	defer resp.Body.Close()
 
-	return ioutil.ReadAll(resp.Body)
+	return io.ReadAll(resp.Body)
 }
 
 func combineErrors(errs []errorObj) error {
